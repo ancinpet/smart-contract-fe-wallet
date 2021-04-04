4cefdafe091c6c1fbdcd37f8405e2a9a2fd3a424
ancinpet <ancinec.petr@hotmail.com>
2021-04-04 16:46:50 +0200

    ViewBind implemented

--- src/thesis-wallet/FormClasses.cs
+++ src/thesis-wallet/FormClasses.cs
@@ -3,6 +3,7 @@ using System.Xml;
 using System.Collections.Generic;
 using System;
 using System.ComponentModel;
+using System.Linq;
 
 namespace thesis_wallet {
     [XmlRootAttribute("Contract")]
@@ -46,7 +47,7 @@ namespace thesis_wallet {
         public List<Field> Fields { get; set; } = new List<Field>();
     }
 
-    public class Field {
+    public abstract class Field {
         [XmlElement("ParamBind")]
         public string ParamBind { get; set; } = "";
         [XmlElement("ViewBind")]
@@ -57,41 +58,108 @@ namespace thesis_wallet {
         public string Description { get; set; } = "";
         [XmlAttribute("ReadOnly")]
         public bool ReadOnly { get; set; } = false;
+
+        public abstract void SetData(string data);
+        public abstract void SetDataList(List<string> data);
     }
 
     public class DateField : Field {
         [XmlIgnoreAttribute]
         public DateTime Data { get; set; }
+
+        public override void SetData(string data) {
+            Data = DateTime.Parse(data);
+        }
+
+        public override void SetDataList(List<string> data) {
+            if (data.Count > 0) {
+                Data = DateTime.Parse(data[0]);
+            }
+        }
     }
 
     public class AddressField : Field {
         [XmlIgnoreAttribute]
         public string Data { get; set; }
+
+        public override void SetData(string data) {
+            Data = data;
+        }
+
+        public override void SetDataList(List<string> data) {
+            Data = string.Join(',', data);
+        }
     }
 
     public class SingleLineField : Field {
         [XmlIgnoreAttribute]
         public string Data { get; set; }
+
+        public override void SetData(string data) {
+            Data = data;
+        }
+
+        public override void SetDataList(List<string> data) {
+            Data = string.Join(',', data);
+        }
     }
 
     public class MultiLineField : Field {
         [XmlIgnoreAttribute]
         public string Data { get; set; }
+
+        public override void SetData(string data) {
+            Data = data;
+        }
+
+        public override void SetDataList(List<string> data) {
+            Data = string.Join(Environment.NewLine, data);
+        }
     }
 
     public class IntField : Field {
         [XmlIgnoreAttribute]
         public long Data { get; set; }
+
+        public override void SetData(string data) {
+            Data = Convert.ToInt64(data);
+        }
+
+        public override void SetDataList(List<string> data) {
+            if (data.Count > 0) {
+                Data = Convert.ToInt64(data[0]);
+            }
+        }
     }
 
     public class DecimalField : Field {
         [XmlIgnoreAttribute]
         public decimal Data { get; set; }
+
+        public override void SetData(string data) {
+            Data = Convert.ToDecimal(data);
+        }
+
+        public override void SetDataList(List<string> data) {
+            if (data.Count > 0) {
+                Data = Convert.ToDecimal(data[0]);
+            }
+        }
     }
 
     public class BoolField : Field {
         [XmlIgnoreAttribute]
         public bool Data { get; set; }
+
+        public override void SetData(string data) {
+            Data = Convert.ToBoolean(data);
+        }
+
+        public override void SetDataList(List<string> data) {
+            if (data.Count > 0) {
+                Data = Convert.ToBoolean(data[0]);
+            }
+        }
     }
 
     public class EnumField : Field {
@@ -99,10 +167,28 @@ namespace thesis_wallet {
         public bool Vertical { get; set; } = false;
         [XmlIgnoreAttribute]
         public List<string> Data { get; set; } = new List<string> { "A", "B", "C"};
+
+        public override void SetData(string data) {
+            Data = new List<string>();
+            Data.Add(data);
+        }
+
+        public override void SetDataList(List<string> data) {
+            Data = data;
+        }
     }
 
     public class DropdownField : Field {
         [XmlIgnoreAttribute]
         public List<string> Data { get; set; } = new List<string> { "A", "B", "C" };
+
+        public override void SetData(string data) {
+            Data = new List<string>();
+            Data.Add(data);
+        }
+
+        public override void SetDataList(List<string> data) {
+            Data = data;
+        }
     }
 }
